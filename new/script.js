b76ff993cf62cf2158e9d7645c466d013d366243
Aka1418 <ak0141904@gmail.com>
2024-07-04 16:24:31 +0500

    Initialize files

---
+++ new/script.js
@@ -0,0 +1,10 @@
+// // Add any interactivity if needed
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Example of adding a click event to each course section
+//     const courses = document.querySelectorAll('.course');
+//     courses.forEach(course => {
+//         course.addEventListener('click', () => {
+//             alert(`You clicked on ${course.querySelector('h2').innerText} course`);
+//         });
+//     });
+// });
